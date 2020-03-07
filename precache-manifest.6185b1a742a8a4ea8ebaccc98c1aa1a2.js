8c69dad2866debfe90e026a8cffa33e79009b75c
James Watson <jameswatsonv@gmail.com>
2020-03-07 01:14:36 -0800

    Updates

---
+++ precache-manifest.6185b1a742a8a4ea8ebaccc98c1aa1a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66a5d0ebfea99405b747ca5bbcabe71",
+    "url": "/hunt-assistant/index.html"
+  },
+  {
+    "revision": "565198667e4db1d840fd",
+    "url": "/hunt-assistant/static/css/main.7dc574fa.chunk.css"
+  },
+  {
+    "revision": "9fba30105f5cca49fb36",
+    "url": "/hunt-assistant/static/js/2.d25e4875.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/hunt-assistant/static/js/2.d25e4875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565198667e4db1d840fd",
+    "url": "/hunt-assistant/static/js/main.a5598567.chunk.js"
+  },
+  {
+    "revision": "951d0aaa2d3ae3934704",
+    "url": "/hunt-assistant/static/js/runtime-main.496d61ce.js"
+  },
+  {
+    "revision": "eb4d5164cb96548598f1317884d31573",
+    "url": "/hunt-assistant/static/media/stop.eb4d5164.svg"
+  }
+]);
